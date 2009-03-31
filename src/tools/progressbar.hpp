06d48b365475c773c94c61ea34e22f1aadd657d2
Hannes Schulz <schulzha@twisted.(none)>
2009-03-31 10:43:34 +0200

    progress bar: predict finish time, clear optionally

--- src/tools/progressbar.hpp
+++ src/tools/progressbar.hpp
@@ -1,5 +1,6 @@
 #ifndef __PROGRESSBAR_HPP__
 #define __PROGRESSBAR_HPP__
+#include "boost/date_time/posix_time/posix_time.hpp"
 #include <iostream>
 #include <string>
 
@@ -13,6 +14,7 @@ class ProgressBar {
        ivCWidth(cwidth),
        ivCPos(-1)
     {
+      ivStartTime = boost::posix_time::second_clock::local_time();
       display();
     }
     inline void inc(char*info,int v=1){
@@ -23,11 +25,16 @@ class ProgressBar {
       ivCurrent += v;
       display();
     }
-    inline void finish(){
-      std::cout << "\r";
-      for(uint i=0;i<ivCWidth + ivClearLen + 20 + ivpDesc.size(); i++)
-	std::cout << " ";
-      std::cout<<"\r"<<std::flush;
+    inline void finish(bool clear=false){
+      if(clear){
+        std::cout << "\r";
+        for(uint i=0;i<std::min((long unsigned int)79,ivCWidth + ivClearLen + 60 + ivpDesc.size()); i++)
+	  std::cout << " ";
+        std::cout<<"\r"<<std::flush;
+      }
+      else{
+        std::cout << std::endl;
+      }
     }
     inline void finish(char* s){
       std::cout << s << std::endl<<std::flush;
@@ -37,12 +44,14 @@ class ProgressBar {
     long int ivMax;
     long int ivCurrent;
     uint ivClearLen;
+    boost::posix_time::ptime ivStartTime;
 
     int ivCWidth;
     int ivCPos;
 
     void display(char* info=""){
-      int newpos = (int)((double)ivCurrent * (double)ivCWidth / (double) ivMax);
+      double newpos_f = ((double)ivCurrent / (double) ivMax);
+      int    newpos   = (int)(ivCWidth * newpos_f);
       if(newpos == ivCPos) return;
 
       ivCPos = newpos;
@@ -52,7 +61,12 @@ class ProgressBar {
        std::cout << "#";
       for(int i=0;i<ivCWidth-newpos;i++)
        std::cout << " ";
-      std::cout << "| [" << (int)(100.0*((double)ivCurrent/(double)ivMax)) << "%] ";
+      std::cout << "| [" << (int)(100.0*newpos_f) << "% (";
+      boost::posix_time::time_duration td=boost::posix_time::time_period(ivStartTime, boost::posix_time::second_clock::local_time()).length();
+      int newsec = (1.0f-newpos_f)/newpos_f * td.total_seconds();
+      td = boost::posix_time::seconds(newsec);
+      boost::posix_time::ptime done(boost::posix_time::second_clock::local_time()+td);
+      std::cout << done <<")] ";
       std::cout << info;
       std::cout << std::flush;
       ivClearLen = ivClearLen>strlen(info)?ivClearLen:strlen(info);
